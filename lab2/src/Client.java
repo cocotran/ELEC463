92215abc7ee49bcadd52fa965c63939854719a93
cocotran <anhquangtran2703@gmail.com>
2021-02-18 22:38:13 -0500

    added lab 2

--- lab2/src/Client.java
+++ lab2/src/Client.java
@@ -1,145 +1,88 @@
+import java.net.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.Socket;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import java.io.*;
 
 public class Client {
 
-    private static JFrame frame = new JFrame();
-    static boolean isConnected = false;
-    private static Socket clientSocket;
-    static int step;
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) {
-        Client client = new Client();
-        step = 0;
-
-//		create frame
-        frame.setBounds(100, 100, 500, 500);
+        JFrame frame = new JFrame();
+        frame.setBounds(600, 200, 600, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("TCP Calculator");
-        frame.getContentPane().setLayout(null); // NOTE: this might cause bugs, textareas and buttons might not appear until mouse over.
+        frame.setTitle("TCP Client");
+        frame.setLayout(null);
         frame.setVisible(true);
 
-        client.displayConnectStatus();
+        //connection status label
+        JLabel statusLabel = new JLabel("Connection Status: Not Connected");
+        statusLabel.setForeground(Color.red);
+        statusLabel.setBounds(20, 20, 500, 20);
+        statusLabel.setFont(new Font("Times", Font.BOLD, 14));
+        frame.getContentPane().add(statusLabel);
 
-//		Connect button
+        //received messages from server in textarea
+        JTextArea recMessages = new JTextArea();
+        recMessages.setBounds(20, 100, 540, 430);
+        frame.getContentPane().add(recMessages);
+        recMessages.setText("Messages received from Server:\n");
+
+        //button to connect or disconnect
         JButton connectButton = new JButton("Connect");
-        connectButton.setBounds(330, 25, 100, 30);
+        connectButton.setBounds(150, 50, 300, 30);
+        connectButton.setFont(new Font("Times", Font.BOLD, 26));
         frame.getContentPane().add(connectButton);
-        connectButton.addActionListener(new ConnectListener());
 
-        if (isConnected && step >= 1) {
-            client.displayCountOfNumber();
-        }
-
-        if (step >= 2) {
-            client.displayAddNumbers();
-        }
-    }
-
-    public void displayConnectStatus() {
-        JLabel label = new JLabel("");
-        label.setBounds(50, 30, 200, 20);
-        label.setFont(new Font("Times", Font.PLAIN, 12));
-        label.setHorizontalAlignment(SwingConstants.LEFT);
-        label.setVerticalAlignment(SwingConstants.CENTER);
-        frame.getContentPane().add(label);
-        if (!this.isConnected) {
-            label.setForeground(Color.red);
-            label.setText("Connection Status: Not Connected");
-        }
-        else {
-            label.setForeground(Color.blue);
-            label.setText("Connection Status: Connected");
-        }
-    }
-
-    public static void connect() {
-        try {
-            clientSocket = new Socket ("localhost", Server.port);
-            isConnected = true;
-            step = 1;
-        } catch (IOException e) {
-            System.out.println("Error: " + e);;
-        }
-    }
-
-    public static void disconnect() {
-        try {
-            clientSocket.close();
-            isConnected = false;
-            step = 0;
-        } catch (IOException e) {
-            System.out.println("Error: " + e);;
-        }
-    }
-
-    public void displayCountOfNumber() {
-        if (isConnected) {
-//		    count numbers
-            JLabel labelCountNumber = new JLabel("Counts of Numbers:");
-            labelCountNumber.setBounds(50, 100, 200, 20);
-            labelCountNumber.setHorizontalAlignment(SwingConstants.LEFT);
-            labelCountNumber.setVerticalAlignment(SwingConstants.CENTER);
-            frame.getContentPane().add(labelCountNumber);
-
-//		    count of numbers input
-            JTextField countOfNumbers = new JTextField("");
-            countOfNumbers.setBounds(200, 100, 100, 30);
-            frame.getContentPane().add(countOfNumbers);
-
-//		    set button
-            JButton setButton = new JButton("Set");
-            setButton.setBounds(330, 100, 100, 30);
-            frame.getContentPane().add(setButton);
-
-            if (step > 1) {
-                // TODO: disable it
+        connectButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(connectButton.getText().equals("Disconnect")) { //if disconnecting
+                    connectButton.setText("Connect");
+                    statusLabel.setText("Connection Status: Not Connected");
+                    statusLabel.setForeground(Color.red);
+                }
+                else { //if connecting
+                    connectButton.setText("Disconnect");
+                    connect(statusLabel, recMessages);
+                }
             }
-            else {
-                labelCountNumber.setFont(new Font("Times", Font.BOLD, 12));
-                countOfNumbers.setFont(new Font("Times", Font.PLAIN, 12));
-            }
+        });
+
+    }
+
+    public static void connect(JLabel statusLabel, JTextArea recMessages) {
+        try {
+
+            //ip address 127.0.0.1 or local address can be used //or on mac/linux loopbackip address
+            Socket clientSocket = new Socket("localhost", 6789);
+
+            statusLabel.setText("Connection Status: Connected");
+            statusLabel.setForeground(Color.blue);
+
+            //send msg to server
+            DataOutputStream outputToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+            String sentence = "Connection Request, "
+                    + "Client Port Number: " + clientSocket.getLocalPort()
+                    + ", Client IP Address: " + clientSocket.getLocalAddress()
+                    + ", Server Port: " + clientSocket.getPort() + '\n'; //have to add \n since using readLine in recSentence
+
+            outputToServer.writeBytes(sentence);
+
+            //read msg from server
+            BufferedReader inputFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String recSentence = inputFromServer.readLine();
+            System.out.println("From Server: " + recSentence);
+            recMessages.append(recSentence + '\n');
+        }
+        catch(Exception ex) {
+
         }
     }
 
-    public void displayAddNumbers() {
-//		add numbers
-        JLabel labelAddNumber = new JLabel("Add Numbers to Message:");
-        labelAddNumber.setBounds(50, 150, 200, 20);
-        labelAddNumber.setFont(new Font("Times", Font.PLAIN, 12));
-        labelAddNumber.setHorizontalAlignment(SwingConstants.LEFT);
-        labelAddNumber.setVerticalAlignment(SwingConstants.CENTER);
-        frame.getContentPane().add(labelAddNumber);
-
-//		add numbers input
-        JTextField addNumber = new JTextField("");
-        addNumber.setFont(new Font("Times", Font.PLAIN, 12));
-        addNumber.setBounds(200, 150, 100, 30);
-        frame.getContentPane().add(addNumber);
-
-//		add button
-        JButton addButton = new JButton("Add");
-        addButton.setBounds(330, 150, 100, 30);
-        frame.getContentPane().add(addButton);;
-    }
-}
-
-class ConnectListener implements ActionListener {
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (Client.isConnected)
-            Client.disconnect();
-        else
-            Client.connect();
-    }
 }
